eba958ed78f7427d12e6ec485ebe2508e2ced80a
fenglex <fenglex@126.com>
2019-08-28 22:29:39 +0800

    Initial commit

---
+++ src/main/java/ink/haifeng/graph/janusgraphvisualization/JanusgraphVisualizationApplication.java
@@ -0,0 +1,13 @@
+package ink.haifeng.graph.janusgraphvisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusgraphVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanusgraphVisualizationApplication.class, args);
+	}
+
+}
