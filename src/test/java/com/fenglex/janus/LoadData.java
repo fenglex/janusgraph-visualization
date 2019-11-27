fe2ed3e6117ef89e42e70986f897a4b578df7300
feng <fenglex@126.com>
2019-11-27 15:53:36 +0800

    修改配置文件配置

--- src/test/java/com/fenglex/janus/LoadData.java
+++ src/test/java/com/fenglex/janus/LoadData.java
@@ -1,13 +1,12 @@
 package com.fenglex.janus;
 
 import cn.hutool.core.io.resource.ClassPathResource;
-import cn.hutool.core.io.resource.ResourceUtil;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 import org.janusgraph.example.GraphOfTheGodsFactory;
 
-import java.net.URL;
+import java.io.File;
 
 /**
  * @Author: haifeng
@@ -17,12 +16,14 @@ import java.net.URL;
 public class LoadData {
     public static void main(String[] args) {
         ClassPathResource classPathResource = new ClassPathResource("janusgraph.properties");
-        String path = classPathResource.getPath();
+        File file = classPathResource.getFile();
+        String path = file.getPath();
         JanusGraph graph = JanusGraphFactory.open(path);
         GraphTraversalSource g = graph.traversal();
         g.V().drop().iterate();
         g.tx().commit();
         graph.tx().commit();
         GraphOfTheGodsFactory.load(graph);
+        graph.close();
     }
 }
