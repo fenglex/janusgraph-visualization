78edf9e5a3a2a60bdcd0acfa75d6934221b54963
feng <fenglex@126.com>
2019-11-27 11:16:58 +0800

    修改docker push脚本

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t janusgrpah-visualization:$1 .
+docker build -t fenglex/janusgrpah-visualization:$1 .
 docker tag janusgrpah-visualization:$1 fenglex/janusgrpah-visualization:latest
-docker push fenglex/janusgrpah-visualization:$1 fenglex/janusgrpah-visualization:latest
-docker push fenglex/janusgrpah-visualization:$1 fenglex/janusgrpah-visualization:$1
+docker push fenglex/janusgrpah-visualization:$1
+docker push fenglex/janusgrpah-visualization:latest
